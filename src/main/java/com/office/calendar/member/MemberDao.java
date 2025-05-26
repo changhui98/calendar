720c0be60868984ed2101cce818dc774ac01fba0
changhui98 <net_0102@naver.com>
2025-05-26 16:18:59 +0900

    [BE] MySQL 데이터베이스 연동 성공

--- src/main/java/com/office/calendar/member/MemberDao.java
+++ src/main/java/com/office/calendar/member/MemberDao.java
@@ -1,23 +1,43 @@
 package com.office.calendar.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberDao {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     public boolean isMember(String id) {
         System.out.println("[MemberDao] isMember()");
 
+        String sql = "SELECT COUNT(*) FROM USER_MEMBER WHERE ID = ?";
 
+        int result = jdbcTemplate.queryForObject(sql, Integer.class, id);
+        if (result > 0) {
+            return true;
+        }
 
         return false;
-    }
+        }
+
 
     public int insertMember(MemberDto memberDto) {
         System.out.println("[MemberDao] insertMember()");
 
+        String sql = "INSERT INTO USER_MEMBER(ID, PW, MAIL, PHONE) VALUES(?, ?, ?, ?)";
 
+        int result = -1;
 
-        return 0;
+        try {
+            result = jdbcTemplate.update(sql, memberDto.getId(), memberDto.getPw(), memberDto.getMail(), memberDto.getPhone());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
+
