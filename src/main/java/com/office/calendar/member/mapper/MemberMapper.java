4fdda51f0c2de11a76558e9461a1661db601bf6e
changhui98 <net_0102@naver.com>
2025-06-15 19:19:00 +0900

    [BE] MyBatis XML 설정에서 중복된 쿼리 SQL 공통 정의 하는 방법 학습

--- src/main/java/com/office/calendar/member/mapper/MemberMapper.java
+++ src/main/java/com/office/calendar/member/mapper/MemberMapper.java
@@ -6,22 +6,29 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface MemberMapper {
 
-    @Select("SELECT COUNT(*) FROM USER_MEMBER WHERE ID = #{id}")
+//    @Select("""
+//            SELECT
+//                COUNT(*)
+//            FROM
+//                USER_MEMBER
+//            WHERE
+//                ID = #{id}
+//            """)
     public boolean isMember(String id);
 
-    @Insert("INSERT INTO USER_MEMBER(ID, PW, MAIL, PHONE) VALUES(#{id}, #{pw}, #{mail}, #{phone}")
+//    @Insert("INSERT INTO USER_MEMBER(ID, PW, MAIL, PHONE) VALUES(#{id}, #{pw}, #{mail}, #{phone}")
     public int insertMember(MemberDto memberdto);
 
-    @Select("SELECT * FROM USER_MEMBER WHERE ID = #{id}")
+//    @Select("SELECT * FROM USER_MEMBER WHERE ID = #{id}")
     public MemberDto selectMemberByID(String id);
 
-    @Update("UPDATE USER_MEMBER SET PW = #{pw}, MAIL = #{mail}, MOD_DATE = NOW() WHERE NO = #{no}")
+//    @Update("UPDATE USER_MEMBER SET PW = #{pw}, MAIL = #{mail}, MOD_DATE = NOW() WHERE NO = #{no}")
     public int updateMember(MemberDto memberdto);
 
-    @Select("SELECT * FROM USER_MEMBER WHERE ID = #{id} AND MAIL = #{mail}")
+//    @Select("SELECT * FROM USER_MEMBER WHERE ID = #{id} AND MAIL = #{mail}")
     public MemberDto selectMemberByIDAndMail(MemberDto memberDto);
 
-    @Update("UPDATE USER_MEMBER SET PW = #{memPw}, MOD_DATE = NOW() WHERE ID = #{memId}")
+//    @Update("UPDATE USER_MEMBER SET PW = #{memPw}, MOD_DATE = NOW() WHERE ID = #{memId}")
     public int updatePassword(@Param("memId") String id,@Param("memPw") String encodeNewPW);
 
 }
