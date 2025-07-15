45f771d4ebd98804c3fd15d1c5e966caac624edc
changhui98 <net_0102@naver.com>
2025-07-15 14:25:50 +0900

    [BE] 오류 해결 *User 객체 패키지 임포트 문제

--- src/main/java/com/office/calendar/config/SecurityConfig.java
+++ src/main/java/com/office/calendar/config/SecurityConfig.java
@@ -1,11 +1,11 @@
 package com.office.calendar.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
